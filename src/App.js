299f140fef5c8e89d57fd02e212f6a63be3a34f7
Priyajha7585 <priyajha7585@gmail.com>
2024-02-20 20:03:53 +0530

    Updated api details

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
 import Coins from './Coins';
 
 function App() {
-  const [searchWord, setSearchWord] = React.useState("");
-  const [listOfCoins, setListOfCoins] = React.useState([]);
-  // function submitData()
-  // {
-  //   Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10", {params:{query:{enteredData}}})
-  //   .then(function(output){
-  //     var myArr = output.data.results;
-  //   })
-  // }
-  // https://api.coinstats.app/public/v1/coins?skip=0&limit=10
-  React.useEffect(
-    ()=>{
-      Axios.get("https://api.coinstats.app/public/v1/coins?skip=0")
-      .then((response)=>{
-        setListOfCoins(response.data.coins)
-      })
-    }
-    ,[])
+  const [searchWord, setSearchWord] = useState("");
+  const [listOfCoins, setListOfCoins] = useState([]);
 
-    const filtered = listOfCoins.filter((coin)=>
-    {
-      return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  useEffect(() => {
+    const API_KEY = 'MOUDA6Qi4RfBWFMIic0JkUxEiRdH4ztiwOKJnvqMaBQ=';
+
+    Axios.get("https://openapiv1.coinstats.app/coins", {
+      headers: {
+        'x-api-key': API_KEY
+      }
     })
+      .then(({data}) => {
+        setListOfCoins(data.result);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
+  const filtered = listOfCoins?.filter((coin) =>
+    coin.name.toLowerCase().includes(searchWord.toLowerCase())
+  );
   return (
     <div className="App">
       <div className='cryptoHeader'>
-        <input type="text" onChange={(event)=>{setSearchWord(event.target.value)}} placeholder="Bitcoin..."/>
-        {/* <button type='submit' >Submit</button> */}
+        <input
+          type="text"
+          value={searchWord}
+          onChange={(event) => setSearchWord(event.target.value)}
+          placeholder="Search..."
+        />
       </div>
       <div className='cryptoDisplay'>
-        {/* {myArr} */}
-        {filtered.map(function(coin)
-        {
-          return <Coins name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol}/>
-        })}
+        {filtered?.map((coin) =>
+          <Coins key={coin.id} name={coin.name} icon={coin.icon} price={coin.price} symbol={coin.symbol} />
+        )}
       </div>
     </div>
   );
